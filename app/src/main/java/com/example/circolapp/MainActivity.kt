36174c5bec348c1cf6b29db088259e6779e9b8a6
matti <mattiatomassetti16@libero.it>
2025-07-01 20:03:31 +0200

    nav bar e schermata home

--- app/src/main/java/com/example/circolapp/MainActivity.kt
+++ app/src/main/java/com/example/circolapp/MainActivity.kt
@@ -1,20 +1,44 @@
 package com.example.circolapp
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, HomeFragment())
+                .commit()
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_home -> {supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, HomeFragment())
+                    .commit()
+                    true }
+                R.id.nav_search -> { /* Naviga a Cerca */ true }
+                R.id.nav_add -> { /* Naviga a Aggiungi */ true }
+                R.id.nav_notifications -> { /* Naviga a Notifiche */ true }
+                R.id.nav_profile -> { /* Naviga a Profilo */ true }
+                else -> false
+            }
+        }
     }
+
 }
+
